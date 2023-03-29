d188563c499c34a3cdac37fc49a842350ddcd179
Phornpitak Kadta <86459508+PumXPer@users.noreply.github.com>
2023-03-29 23:36:20 +0700

    xv

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'projectXV'`, () => {
+  it(`should have as title 'projectX'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projectXV');
+    expect(app.title).toEqual('projectX');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('projectXV app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('projectX app is running!');
   });
 });
